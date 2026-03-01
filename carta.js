8ccba230b13e36708c00cac2e46c3cd0fb6468d8
erickmanzanoquerido-wq <erickmanzanoquerido@gmail.com>
2026-02-28 21:22:19 -0500

    Delete carta.js

--- carta.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("abrirBtn").onclick = function() {
-    document.getElementById("carta").style.display = "block";
-    document.getElementById("abrirBtn").style.display = "none";
-};
